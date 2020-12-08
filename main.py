ed0bd4057558ad3e58e9bc3ee9d0445f9b51bf90
anaya5837 <72808504+anaya5837@users.noreply.github.com>
2020-12-08 11:02:15 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+light.set_brightness(100)
 while True:
-    print("Light Level: " + input.light_level())
+    print("Light Level: " + input.light_level()) 
+    if input.light_level() < 10:
+        light.set_all(light.rgb(100, 100, 100))
+    else: 
+        light.clear
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+light.setBrightness(100)
 while (true) {
     console.log("Light Level: " + input.lightLevel())
+    if (input.lightLevel() < 10) {
+        light.setAll(light.rgb(100, 100, 100))
+    } else {
+        light.clear
+    }
+    
 }
